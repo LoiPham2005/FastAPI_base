a79e9bcd1c587ef8dda13f1c1e65665cd7b2bc5e
LoiPham2005 <phamducloi919@gmail.com>
2026-02-20 16:48:15 +0700

    update

--- app/api/v1/bookings.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/")
-def get_bookings():
-    return {"message": "List of bookings"}
--- app/api/v1/venues.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/")
-def get_venues():
-    return {"message": "List of venues"}
